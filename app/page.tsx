5795a9a12f9ca0d501e2c151e2341ab786148473
daksh <akshd@outlook.com>
2025-04-28 12:43:05 +0530

    feat: add fc sdk

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,23 @@
+"use client"
+
 import SnapCamera from "@/components/SnapCamera";
+import sdk from "@farcaster/frame-sdk";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const [loaded, setLoaded] = useState(false)
+
+  useEffect(() => {
+    if (!loaded) {
+      setLoaded(true)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (loaded) sdk.actions.ready()
+  }, [loaded])
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <SnapCamera />
