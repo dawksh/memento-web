22d379e76f8b5050cca39d01c3f10a27dc4bc0cb
daksh <akshd@outlook.com>
2025-04-29 00:19:39 +0530

    chore: add avatar

--- components/Feed/RightSection.tsx
+++ components/Feed/RightSection.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { usePrivy } from '@privy-io/react-auth';
+import { shortenAddress } from '@/lib/utils';
+import Link from 'next/link';
+import { PencilIcon } from 'lucide-react';
 
 const RightSection = () => {
+  const { user } = usePrivy();
+
   return (
-    <div className="w-1/4 h-screen p-4">
-      {/* Content will be added later */}
+    <div className="w-1/4 h-screen p-4 sticky top-0">
+      {user && (
+        <div className="bg-background p-4 rounded-lg shadow-lg">
+          <div className="flex items-center space-x-4">
+            <img
+              className="w-16 h-16 rounded-full"
+              src={`https://avatar.iran.liara.run/public?address=${user.wallet?.address}`}
+              alt="Profile picture"
+            />
+            <div className="flex flex-col">
+              <div className="text-lg font-bold">{user.farcaster?.displayName}</div>
+              <div className="text-sm text-muted-foreground">
+                {shortenAddress(user.wallet?.address!)}
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- components/Shared/Navbar.tsx
+++ components/Shared/Navbar.tsx
@@ -6,12 +6,12 @@ import { usePrivy } from '@privy-io/react-auth'
 import { shortenAddress } from '@/lib/utils'
 
 const Navbar = () => {
-    const { login, authenticated, user } = usePrivy()
+    const { login, logout, authenticated, user } = usePrivy()
     return (
         <div className='flex flex-row justify-between p-4'>
             <div className='italic font-semibold'>memento</div>
             <div>
-                {authenticated ? <>{shortenAddress(user?.wallet?.address!)}</> : <Button className='hover:cursor-pointer' onClick={login}>login</Button>}
+                {authenticated ? <><Button onClick={logout}>logout</Button></> : <Button className='hover:cursor-pointer' onClick={login}>login</Button>}
             </div>
         </div>
     )
