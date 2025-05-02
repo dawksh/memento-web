0929e8b3d6a3454b26b543a1bf0496500f824f7d
daksh <akshd@outlook.com>
2025-05-02 23:30:57 +0530

    chore: fix navbar

--- components/Shared/Navbar.tsx
+++ components/Shared/Navbar.tsx
@@ -7,7 +7,7 @@ import { usePrivy } from '@privy-io/react-auth'
 const Navbar = () => {
     const { login, logout, authenticated, user } = usePrivy()
     return (
-        <div className='flex flex-row justify-between p-4'>
+        <div className='flex flex-row justify-between items-center p-4'>
             <div className='italic font-semibold'>memento</div>
             <div>
                 {authenticated ? <><Button onClick={logout}>logout</Button></> : <Button className='hover:cursor-pointer' onClick={login}>login</Button>}
