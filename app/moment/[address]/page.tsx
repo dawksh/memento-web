52f3f630cf8a6415562d72fb86f09245f827f825
daksh <akshd@outlook.com>
2025-05-21 21:06:11 +0530

    chore: ready fc sdk

--- app/moment/[address]/page.tsx
+++ app/moment/[address]/page.tsx
@@ -1,10 +1,24 @@
 "use client"
 import { useParams } from 'next/navigation'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useMoments } from '@/hooks/useMoments'
 import Post from '@/components/Feed/Post'
+import sdk from '@farcaster/frame-sdk'
 
 const Page = () => {
+
+    const [loaded, setLoaded] = useState(false)
+
+    useEffect(() => {
+        if (!loaded) {
+            setLoaded(true)
+        }
+    }, [])
+
+    useEffect(() => {
+        if (loaded) sdk.actions.ready()
+    }, [loaded])
+
     const { address } = useParams()
     const { data: moments } = useMoments({ coin: address as string })
     return (
